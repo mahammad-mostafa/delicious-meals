1cde8a427e86373dca9fe9108da1be1466d952da
Felipe <fperezferraro@gmail.com>
2023-08-02 11:53:38 -0300

    creating the dom.js file with all the Ids and selectors

---
+++ src/modules/dom.js
@@ -0,0 +1,39 @@
+const popupSection = document.getElementById('popup_section');
+const closeButton = document.getElementById('close_button');
+const mealImage = document.getElementById('meal_image');
+const mealTitle = document.getElementById('meal_title');
+const mealDescription = document.getElementById('meal_description');
+const subTitleAction = document.getElementById('sub_title_action');
+const dateList = document.getElementById('date_list');
+const addActionTitle = document.getElementById('add_action_title');
+const commentForm = document.getElementById('comment_form');
+const reservationForm = document.getElementById('reservation_form');
+const commentUsername = document.getElementById('comment_username');
+const commentArea = document.getElementById('comment_area');
+const submitCommentBtn = document.getElementById('submit_comment_btn');
+const reservationUsername = document.getElementById('reservation_username');
+const reservationDateStart = document.getElementById('reservation_date_start');
+const reservationDateEnd = document.getElementById('reservation_date_end');
+const submitReservationBtn = document.getElementById('submit_reservation_btn');
+const itemId = document.getElementById('item_id');
+
+export {
+  popupSection,
+  closeButton,
+  mealImage,
+  mealTitle,
+  mealDescription,
+  subTitleAction,
+  dateList,
+  addActionTitle,
+  commentForm,
+  reservationForm,
+  commentUsername,
+  commentArea,
+  submitCommentBtn,
+  reservationUsername,
+  reservationDateStart,
+  reservationDateEnd,
+  submitReservationBtn,
+  itemId,
+};
