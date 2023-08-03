5beb52b21c47f96d6fdc31cb7633559163ded71b
Felipe <fperezferraro@gmail.com>
2023-08-03 14:15:49 -0300

    changing the structure of the Reservations class

--- src/modules/reservations.js
+++ src/modules/reservations.js
@@ -1,24 +1,13 @@
-import FormManagement from './formManagement';
-import Dom from './dom';
-
-export default class {
-  constructor(network) {
-    this.formManagement = new FormManagement();
-    this.dom = new Dom();
+export default class Reservations{
+  constructor(network, itemId, username, dateStart, dateEnd) {
     this.network = network;
+    this.itemId = itemId;
+    this.username = username;
+    this.dateStart = dateStart;
+    this.dateEnd = dateEnd;
   }
 
   postReservationMethod = () => {
-    if (this.formManagement.checkFilledForm('popup-form-reservation')) {
-      const reservationObject = {
-        item_id: data.itemDetails.item_id,
-        username: this.dom.popupFormReservation.elements.username.value,
-        date_start: this.dom.popupFormReservation.elements.date_start.value,
-        date_end: this.dom.popupFormReservation.elements.date_end.value,
-      };
-      this.network.postReservation(reservationObject.item_id, reservationObject.username, reservationObject.date_start, reservationObject.date_end);
-    } else {
-      throw new Error('Invalid reservation Post');
-    }
+    this.network.postReservation(this);
   }
 }
