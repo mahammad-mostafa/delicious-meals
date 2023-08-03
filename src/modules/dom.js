ff74e33fae89e2915b9d2382438388ffa4c2fcea
Felipe <fperezferraro@gmail.com>
2023-08-03 12:46:04 -0300

    adding all the dom elements in this file

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -1,39 +1,17 @@
-const popupSection = document.getElementById('popup_section');
-const closeButton = document.getElementById('close_button');
-const mealImage = document.getElementById('meal_image');
-const mealTitle = document.getElementById('meal_title');
-const mealDescription = document.getElementById('meal_description');
-const subTitleAction = document.getElementById('sub_title_action');
-const dateList = document.getElementById('date_list');
-const addActionTitle = document.getElementById('add_action_title');
-const commentForm = document.getElementById('comment_form');
-const reservationForm = document.getElementById('reservation_form');
-const commentUsername = document.getElementById('comment_username');
-const commentArea = document.getElementById('comment_area');
-const submitCommentBtn = document.getElementById('submit_comment_btn');
-const reservationUsername = document.getElementById('reservation_username');
-const reservationDateStart = document.getElementById('reservation_date_start');
-const reservationDateEnd = document.getElementById('reservation_date_end');
-const submitReservationBtn = document.getElementById('submit_reservation_btn');
-const itemId = document.getElementById('item_id');
+class Dom {
+  constructor() {
+    this.popup = document.getElementById('popup');
+    this.popupBlock = document.getElementById('popup-block');
+    this.popupClose = document.getElementById('popup-close');
+    this.popupImage = document.getElementById('popup-image');
+    this.popupTitle = document.getElementById('popup-title');
+    this.popupDescription = document.getElementById('popup-description');
+    this.popupListTitle = document.getElementById('popup-list-title');
+    this.popupList = document.getElementById('popup-list');
+    this.popupFormTitle = document.getElementById('popup-form-title');
+    this.popupFormComment = document.getElementById('popup-form-comment');
+    this.popupFormReservation = document.getElementById('popup-form-reservation');
+  }
+}
 
-export {
-  popupSection,
-  closeButton,
-  mealImage,
-  mealTitle,
-  mealDescription,
-  subTitleAction,
-  dateList,
-  addActionTitle,
-  commentForm,
-  reservationForm,
-  commentUsername,
-  commentArea,
-  submitCommentBtn,
-  reservationUsername,
-  reservationDateStart,
-  reservationDateEnd,
-  submitReservationBtn,
-  itemId,
-};
+export default Dom;
