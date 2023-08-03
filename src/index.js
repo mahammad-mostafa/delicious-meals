6fb2cbed4cf7d1a41f5c88630ff42b749bbc37d6
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2023-08-03 19:36:59 +0300

    Used datasets instead of id attributes for buttons

--- src/index.js
+++ src/index.js
@@ -8,18 +8,20 @@ const network = new Network(menuElement, listElement);
 
 const menuEvent = (event) => {
   event.preventDefault();
-  if (event.target.hash !== undefined && ['#seafood', '#pasta', '#vegan'].includes(event.target.hash)) {
-    network.getList(event.target.hash.replace('#', ''));
+  const menuHash = event.target.hash;
+  if (menuHash !== undefined && ['#seafood', '#pasta', '#vegan'].includes(menuHash)) {
+    network.getList(menuHash.replace('#', ''));
   }
 };
 
 const listEvent = (event) => {
-  if (event.target.id !== undefined && ['likes', 'comments', 'reservations'].includes(event.target.id)) {
+  const buttonType = event.target.dataset.type;
+  if (buttonType !== undefined && ['likes', 'comments', 'reservations'].includes(buttonType)) {
     const itemId = event.target.parentNode.id.replace('item', '');
-    if (event.target.id === 'likes') {
+    if (buttonType === 'likes') {
       network.postLike(itemId);
     } else {
-      network.openPopup(event.target.id, itemId);
+      network.openPopup(buttonType, itemId);
     }
   }
 };
