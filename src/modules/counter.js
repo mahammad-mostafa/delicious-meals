8d4c18fb3348eb62dbc70bb22c5ba64998906d9a
kambereBr <brunokambere@gmail.com>
2023-08-05 00:25:05 +0200

    fix linters formatting errors

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -1,6 +1,6 @@
-export default counter = (popupList) => {
-    if (popupList === null || popupList === undefined) {
-        return 0;
-    }
-    return popupList.childElementCount;
+export default (popupList) => {
+  if (popupList === null || popupList === undefined) {
+    return 0;
+  }
+  return popupList.childElementCount;
 };
--- src/tests/comments.test.js
+++ src/tests/comments.test.js
@@ -1,34 +1,25 @@
+import counter from '../modules/counter.js';
+
 const { expect } = require('@jest/globals');
-import counter from '../modules/counter';
 
 describe('Test comments counter function', () => {
-    test('counter function with an null param returns 0', () => {
-        expect(counter(null)).toBe(0);
-    });
+  test('counter function with an null param returns 0', () => {
+    expect(counter(null)).toBe(0);
+  });
 
-    test('counter function with an undefined param returns 0', () => {
-        expect(counter(undefined)).toBe(0);
-    });
+  test('counter function with an undefined param returns 0', () => {
+    expect(counter(undefined)).toBe(0);
+  });
 
-    test('counter function with a HTMLElement that has no child Element returns 0', () => {
-        document.body.innerHTML = `<ul id='list'></ul>`;
-        const list = document.getElementById('list');
-        expect(counter(list)).toBe(0);
-    });
+  test('counter function with a HTMLElement that has no child Element returns 0', () => {
+    document.body.innerHTML = '<ul id=\'list\'></ul>';
+    const list = document.getElementById('list');
+    expect(counter(list)).toBe(0);
+  });
 
-    test('counter function with a HTMLElement that has 2 children Elements returns 2', () => {
-        document.body.innerHTML = `<ul id='list'><li>One Comment</li><li>Two Comments</li></ul>`;
-        const list = document.getElementById('list');
-        expect(counter(list)).toBe(2);
-    });
-})
-
-
-
-
-
-
-
-
-
-
+  test('counter function with a HTMLElement that has 2 children Elements returns 2', () => {
+    document.body.innerHTML = '<ul id=\'list\'><li>One Comment</li><li>Two Comments</li></ul>';
+    const list = document.getElementById('list');
+    expect(counter(list)).toBe(2);
+  });
+});
